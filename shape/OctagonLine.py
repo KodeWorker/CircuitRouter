f50d9fba5594bc3d7e2b22511148b0b3cfae745a
kodeworker <fxp61005@gmail.com>
2019-05-13 00:28:36 +0000

    20190513 update

--- shape/OctagonLine.py
+++ shape/OctagonLine.py
@@ -7,106 +7,118 @@ content:
     - solid_octagon_line
 
 author: Shin-Fu (Kelvin) Wu
-latest update: 2019/05/10
+latest update: 
+    - 2019/05/10
+    - 2019/05/13
 """
 from math import ceil, sqrt
 from Octagon import octagon, solid_octagon
 from Rectangle import (rectangle, diagonal_rectangle, solid_rectangle,
                        solid_diagonal_rectangle)
 
-def octagon_line(start, goal, r):
+def octagon_line(start, goal, r, is_with_start=True, is_with_goal=True):
     pixel = set()
+    a = int(ceil(r*sqrt(2)/(2+sqrt(2))))
+    r = int(ceil(r))
+    
     dx = abs(start[0] - goal[0])
     dy = abs(start[1] - goal[1])
     if dx == dy:
-        # Diagonal
-        a = int(ceil(r*sqrt(2)/(2+sqrt(2))))
-        r = int(ceil(r))
+        # Diagonal                
         if start[1] >= goal[1]:
             if start[0] >= goal[0]:
-                pt1 = (goal[0] + a, goal[1] - r)
-                pt2 = (start[0] - a, start[1] + r)
+                pt1 = (goal[0] + r, goal[1] - a)
+                pt2 = (start[0] - r, start[1] + a)
             else:
-                pt1 = (goal[0] - a, goal[1] - r)
-                pt2 = (start[0] + a, start[1] + r) 
+                pt1 = (goal[0] - r, goal[1] - a)
+                pt2 = (start[0] + r, start[1] + a) 
         else:
             if start[0] >= goal[0]:
-                pt1 = (start[0] - a, start[1] - r)
-                pt2 = (goal[0] + a, goal[1] + r)
+                pt1 = (start[0] - r, start[1] - a)
+                pt2 = (goal[0] + r, goal[1] + a)
             else:
-                pt1 = (start[0] + a, start[1] - r)
-                pt2 = (goal[0] - a, goal[1] + r)            
+                pt1 = (start[0] + r, start[1] - a)
+                pt2 = (goal[0] - r, goal[1] + a)            
         
-        oct1 = octagon(start[0], start[1], r)
-        oct2 = octagon(goal[0], goal[1], r)
         rect = diagonal_rectangle(pt1, pt2)
-        pixel |= oct1 | oct2 | rect
+        pixel |= rect
     elif dx == 0 and dy != 0:
         # Vertical
         pt1y = min(start[1], goal[1])
         pt2y = max(start[1], goal[1])
-        oct1 = octagon(start[0], pt1y, r)
-        oct2 = octagon(start[0], pt2y, r)
-        rect = rectangle((start[0]-r, pt1y), (start[1]+r, pt2y))
-        pixel |= oct1 | oct2 | rect
+        rect = rectangle((start[0]-r, pt1y+a), (start[1]+r, pt2y-a))
+        pixel |= rect
     elif dy == 0 and dx != 0:        
         # Horizontal
         pt1x = min(start[0], goal[0])
         pt2x = max(start[0], goal[0])
-        oct1 = octagon(pt1x, start[1], r)
-        oct2 = octagon(pt2x, start[1], r)
-        rect = rectangle((pt1x, start[1]-r), (pt2x, start[1]+r))
-        pixel |= oct1 | oct2 | rect
+        rect = rectangle((pt1x+a, start[1]-r), (pt2x-a, start[1]+r))
+        pixel |= rect
     else:
         raise ValueError("Not a valid line statement")
     
+    if is_with_start:
+        oct1 = octagon(start[0], start[1], r)
+    else:
+        oct1 = set()
+    if is_with_goal:
+        oct2 = octagon(goal[0], goal[1], r)
+    else:
+        oct2 = set()
+    pixel |= oct1 | oct2
+    
     return pixel
 
-def solid_octagon_line(start, goal, r):
+def solid_octagon_line(start, goal, r, is_with_start=True, is_with_goal=True):
     pixel = set()
+    a = int(ceil(r*sqrt(2)/(2+sqrt(2))))
+    r = int(ceil(r))
+    
     dx = abs(start[0] - goal[0])
     dy = abs(start[1] - goal[1])
     if dx == dy:
-        # Diagonal
-        a = int(ceil(r*sqrt(2)/(2+sqrt(2))))
-        r = int(ceil(r))
+        # Diagonal        
         if start[1] >= goal[1]:
             if start[0] >= goal[0]:
-                pt1 = (goal[0] + a, goal[1] - r)
-                pt2 = (start[0] - a, start[1] + r)
+                pt1 = (goal[0] + r, goal[1] - a)
+                pt2 = (start[0] - r, start[1] + a)
             else:
-                pt1 = (goal[0] - a, goal[1] - r)
-                pt2 = (start[0] + a, start[1] + r) 
+                pt1 = (goal[0] - r, goal[1] - a)
+                pt2 = (start[0] + r, start[1] + a) 
         else:
             if start[0] >= goal[0]:
-                pt1 = (start[0] - a, start[1] - r)
-                pt2 = (goal[0] + a, goal[1] + r)
+                pt1 = (start[0] - r, start[1] - a)
+                pt2 = (goal[0] + r, goal[1] + a)
             else:
-                pt1 = (start[0] + a, start[1] - r)
-                pt2 = (goal[0] - a, goal[1] + r)
+                pt1 = (start[0] + r, start[1] - a)
+                pt2 = (goal[0] - r, goal[1] + a)
                 
-        oct1 = solid_octagon(start[0], start[1], r)
-        oct2 = solid_octagon(goal[0], goal[1], r)
         rect = solid_diagonal_rectangle(pt1, pt2)
-        pixel |= oct1 | oct2 | rect
+        pixel |= rect
     elif dx == 0 and dy != 0:
         # Vertical
         pt1y = min(start[1], goal[1])
         pt2y = max(start[1], goal[1])
-        oct1 = solid_octagon(start[0], pt1y, r)
-        oct2 = solid_octagon(start[0], pt2y, r)
-        rect = solid_rectangle((start[0]-r, pt1y), (start[1]+r, pt2y))
-        pixel |= oct1 | oct2 | rect
+        rect = solid_rectangle((start[0]-r, pt1y+a), (start[1]+r, pt2y-a))
+        pixel |= rect
     elif dy == 0 and dx != 0:        
         # Horizontal
         pt1x = min(start[0], goal[0])
         pt2x = max(start[0], goal[0])
-        oct1 = solid_octagon(pt1x, start[1], r)
-        oct2 = solid_octagon(pt2x, start[1], r)
-        rect = solid_rectangle((pt1x, start[1]-r), (pt2x, start[1]+r))
-        pixel |= oct1 | oct2 | rect
+        rect = solid_rectangle((pt1x+a, start[1]-r), (pt2x-a, start[1]+r))
+        pixel |= rect
     else:
         raise ValueError("Not a valid line statement")
+    
+    if is_with_start:
+        oct1 = solid_octagon(start[0], start[1], r)
+    else:
+        oct1 = set()
+    if is_with_goal:
+        oct2 = solid_octagon(goal[0], goal[1], r)
+    else:
+        oct2 = set()
+    pixel |= oct1 | oct2
     
     return pixel
 
@@ -143,11 +155,11 @@ if __name__ == '__main__':
         plt.scatter(pos[0], pos[1], color='blue')
         
     for pos in ol1:
-        plt.scatter(pos[0], pos[1], color='red')
+        plt.scatter(pos[0]+0.1, pos[1]+0.1, color='red')
     for pos in ol2:
-        plt.scatter(pos[0], pos[1], color='red')
+        plt.scatter(pos[0]+0.1, pos[1]+0.1, color='red')
     for pos in ol3:
-        plt.scatter(pos[0], pos[1], color='red')        
+        plt.scatter(pos[0]+0.1, pos[1]+0.1, color='red')        
     plt.savefig('octagon_line.png', dpi=200)
     plt.close()
     
